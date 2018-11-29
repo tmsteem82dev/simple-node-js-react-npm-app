75b8a8afd48a3596182935e23a18b98e6d7d8e33
tims <tmdev82@gmail.com>
2018-11-29 21:27:57 +0000

    changed text in App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to continues intergration tests with jenkins!</h1>
         </header>
+	<p>
+	    This is pretty cool huh?
+	</p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
